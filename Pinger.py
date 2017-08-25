fcbe1b83e348bf3a0f7d2631fb8d6b155e079dde
Michael Crinite <mikecrinite@gmail.com>
2017-08-24 21:30:52 -0400

    changes

--- Pinger.py
+++ Pinger.py
@@ -14,17 +14,29 @@ import smtplib
 from email import mime
 from email.mime import multipart
 from email.mime.text import MIMEText
+from credentials import get_creds
+import logging
+import os
+import platform
+
+logger = logging.getLogger('Pinger')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='Pinger.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s: %(message)s'))
+logger.addHandler(handler)
 
 # Name of server/website/computer name to ping
-hostname = input("Enter IP address of server: \n")
+hostname = get_creds('hostname')
 # Username for email account to log into
-username = input("Enter username: ")
+username = get_creds('username')
 # Password for email account to log into
-password = input("Enter password: ");
+password = get_creds('password')
 # Address to send mail from
-fromaddr = input("Enter address to send from: ")
+fromaddr = get_creds('fromaddr')
 # Address to send mail to (@att.txt.net)
-toaddrs = input("Enter address to send to: ")
+toaddr = get_creds('toaddr')
+
+plat = platform.system().lower()
 
 
 def pingserv():
@@ -32,22 +44,10 @@ def pingserv():
     Pings the server
     Returns True if the server is online, False if the server is offline
     """
-    # Determine what OS the program is running on
-    import os, platform, subprocess
-    plat = platform.system().lower()
-    ping_format = ""
-    if plat == "windows":
-        ping_format = "-n"
-    else:
-        ping_format = "-c"
-
-    # ping = os.system("ping " + ping_format + " 1 " + hostname)
-    with open(os.devnull, 'w') as DEVNULL:
-        try:
-            subprocess.check_call(['ping', ping_format, '1', hostname], stdout=DEVNULL, stderr=DEVNULL)
-            return True
-        except subprocess.CalledProcessError:
-            return False
+    ping_format = "-n" if plat == "windows" else "-c"
+    ping = os.system("ping " + ping_format + " 1 " + hostname)
+    logger.info("Pinged " + hostname)
+    return ping == 0
 
 
 def send(subject, body):
@@ -55,7 +55,7 @@ def send(subject, body):
     # Format message
     msg = mime.multipart.MIMEMultipart()
     msg['From'] = fromaddr
-    msg['To'] = toaddrs
+    msg['To'] = toaddr
     msg['Subject'] = subject
     msg.attach(mime.text.MIMEText(body, 'plain'))
     message = msg.as_string()
@@ -65,53 +65,31 @@ def send(subject, body):
     server.ehlo()
     server.starttls()
     server.login(username, password)
-    server.sendmail(fromaddr, toaddrs, message)
+    server.sendmail(fromaddr, toaddr, message)
     server.quit()
 
 
 def online():
     """ Reports twice per hour that the server is responding to a ping test """
-    if datetime.now().minute % 30 == 0:
-        subject = "Server online"
-        msg = "    " + hostname + " is online at: " + str(datetime.now())
-        print(msg)
-        send(subject, msg)
-        time.sleep(300)
-    return True
-
-
-def onlinedebugmode():
-    """
-    Reports once per minute that the server is responding to a ping test so that
-    You don't have to wait a half hour for the response via online()
-
-    :return: True
-    """
-    if datetime.now().second & 60 == 0:
-        subject = "Server online"
-        msg = "    " + hostname + " is online at: " + str(datetime.now())
-        print(msg)
-        send(subject, msg)
-        time.sleep(30)
-    return True
+    #if datetime.now().minute % 30 == 0:
+    msg = "    " + hostname + " is online at: " + str(datetime.now())
+    logger.info(msg)
+    send("Server online", msg)
 
 
 def offline():
-    """ Reports once per minute that the server is not responding to a ping test """
-    if datetime.now().second % 60 == 0:
-        subject = "Server OFFLINE"
-        msg = "(!) " + hostname + " is OFFLINE at: " + str(datetime.now())
-        # print("(!)", hostname, "is OFFLINE at: ", datetime.now())
-        print(msg)
-        send(subject, msg)
-        time.sleep(30)
-    return True
+    """ Reports once per minute whether or not the server is responding to a ping test """
+    #if datetime.now().second % 60 == 0:
+    msg = "(!) " + hostname + " is OFFLINE at: " + str(datetime.now())
+    logger.error(msg)
+    send("Server OFFLINE", msg)
 
 
 while True:
     """ Tries a ping test to the server. Reports whether it was successful or not """
     if pingserv():
         online()
-        # onlinedebugmode()
+        time.sleep(300)
     else:
         offline()
+        time.sleep(30)
